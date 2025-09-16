03237b18e7da6f8d65852c3ab8441bd9ac73be4d
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-09-16 23:53:46 +0900

    Drill #2 제출 - 2021180011 박준기

--- character_grass.py
+++ character_grass.py
@@ -1,6 +1,9 @@
-from operator import truediv
 
 from pico2d import *
+import math
+
+nDegree=0
+nSpinTimes=0
 
 open_canvas()
 
@@ -11,27 +14,34 @@ y=90
 Direction=4
 
 while(1):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, y)
-    if(x==800 and y==90):
-        Direction=1
-    elif(x==800 and y==600):
-        Direction=2
-    elif(x==0 and y==600):
-        Direction=3
-    elif(x==0 and y==90):
-        Direction=4
-
-    if(Direction==1):
-        y+=2
-    elif(Direction==2):
-        x-=2
-    elif(Direction==3):
-        y-=2
-    elif(Direction==4):
-        x+=2
-
     delay(0.01)
+    if(nSpinTimes%2==0):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        if(x==760 and y==90):
+            Direction=1
+            character=load_image('character_U.png')
+        elif(x==760 and y==560):
+            Direction=2
+            character=load_image('character_L.png')
+        elif(x==30 and y==560):
+            Direction=3
+            character=load_image('character_D.png')
+        elif(x==30 and y==90):
+            Direction=4
+            character=load_image('character_R.png')
+
+        if(Direction==1):
+            y+=2
+        elif(Direction==2):
+            x-=2
+        elif(Direction==3):
+            y-=2
+        elif(Direction==4):
+            x+=2
+
+
+
 
 close_canvas()
