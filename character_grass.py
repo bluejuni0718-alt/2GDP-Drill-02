2872303808ab42907f2fe346d0fd4f25af1c5fa6
bluejuni0718-alt <bluejuni718@tukorea.ac.kr>
2025-09-16 18:17:13 +0900

    add png

--- character_grass.py
+++ character_grass.py
@@ -1,16 +1,39 @@
+from operator import truediv
+import pygame
 from pico2d import *
 
 open_canvas()
 
 grass=load_image('grass.png')
-character=load_image('character.png')
+character=load_image('character_R.png')
+x=400
+y=90
+Direction=4
 
-x=800
-while(x>0):
+
+
+while(1):
     clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x=x-2
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    if(x==800 and y==90):
+        Direction=1
+    elif(x==800 and y==600):
+        Direction=2
+    elif(x==0 and y==600):
+        Direction=3
+    elif(x==0 and y==90):
+        Direction=4
+
+    if(Direction==1):
+        y+=2
+    elif(Direction==2):
+        x-=2
+    elif(Direction==3):
+        y-=2
+    elif(Direction==4):
+        x+=2
+
     delay(0.01)
 
 close_canvas()
